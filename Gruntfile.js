33905064cc0954aacfcc70479a0c8b272af02611
Horgeon <me@horgeon.fr>
2018-06-27 23:19:59 +0200

    Removed useless dependency and task, related to SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -142,61 +142,6 @@ module.exports = function (grunt) {
     grunt.config('buildtheme', theme);
   });
 
-  /**
-  * Regex borrowed form
-  * https://gist.github.com/rosskevin/ddfe895091de2ca5f931
-  * */
-  grunt.registerTask('convert_less', 'Convert less to scss using regular expression', function (theme) {
-    var convertBaseDir = '';
-    grunt.file.expand(convertBaseDir + (theme ? ('*' + theme) : '*') + '/*.less').forEach(function (lessFile) {
-      if (lessFile !=="global/build.less"){
-        var srcContents = grunt.file.read(lessFile);
-        var out = srcContents
-                // 1.  replace @ with $
-                .replace(/@(?!import|media|keyframes|-)/g, '$')
-                // 2.  replace mixins
-                .replace(/[\.#](?![0-9])([\w\-]*)\s*\((.*)\)\s*\{/g, '@mixin $1($2){')
-                // 3.  In LESS, bootstrap namespaces mixins, in SASS they are just prefixed e.g #gradient > .vertical-three-colors becomes @include gradient-vertical-three-colors
-                .replace(/[\.#](?![0-9])([\w\-]*)\s>\s\.(.*;)/g, '@include $1-$2')
-                // 4.  replace includes
-                .replace(/[\.#](?![0-9])([\w\-].*\(.*;)/g, '@include $1')
-                // 5.  replace no param mixin includes with empty parens
-                .replace(/@include\s([\w\-]*\s*);/g, '@include $1();')
-                // 6.  replace extends .class; @extend .class;
-                .replace(/(\.(?![0-9])([\w\-]+);)/g, '@extend $1')
-                // 7.  replace string literals
-                .replace(/~"(.*)"/g, '#{"$1"}')
-                // 8.  replace interpolation  ${var} > #{$var} 
-                .replace(/\$\{(.*)\}/g, '#{$$$1}')
-                // 9.  replace spin to adjust-hue (function name diff)
-                .replace(/spin\(/g, 'adjust-hue(')
-                // 10. replace bower and imports in build.scss
-                .replace(/bootstrap\/less\//g, 'bootstrap-sass-official/assets/stylesheets/')
-                .replace(/\.less/g, '')
-                // 11. replace icon-font-path value with conditional for asset helpers
-                .replace(/(\$icon-font-path:).*;/g, '$1 if($bootstrap-sass-asset-helper, "bootstrap/", "../fonts/bootstrap/");')
-                // 12. set bootswatch's web-font-path value as !default
-                .replace(/(\$web-font-path:.*);/g, '$1 !default;')
-                // 13. Remove the web-font mixin which breaks in libsass
-                .replace(/[\s\S][\s\S]@mixin web-font.*([\s\S]*?).*;([\s\S]*?)}([\s\S]*?)/,"")
-                // 14. Replace usage of the web-font mixin with variable interpolation
-                .replace(/@include web-font/,'@import url');
-
-                if (/\/variables.less$/.test(lessFile)) {
-                // 15. set default value of $bootstrap-sass-asset-helper to false
-                  out = "$bootstrap-sass-asset-helper: false;\n" + out;
-                // 16. only assign variables if they haven't been previously set e.g. $var: #f00; > $var: #f00 !default;
-                  out = out.replace(/^(\$.*);/gm, '$1 !default;');
-                }
-
-        var baseDirRegex = new RegExp("^" + convertBaseDir, "g");
-        var sassFile = lessFile.replace(baseDirRegex, '').replace(/\.less$/, '.scss').replace(/(bootswatch|variables)/, '_$1');
-        grunt.file.write(sassFile, out);
-        grunt.log.writeln('Converted less file:  ', lessFile, Array(27 - lessFile.length).join(' '),'> ', sassFile);
-      }
-    });
-  });
-
   grunt.registerTask('server', 'connect:keepalive');
 
   grunt.registerTask('default', ['connect:base', 'watch']);
